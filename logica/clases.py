7de6a23c0866f22b8510916db7a5f7623ecdea43
Luis Maldonado <lm271022@gmail.com>
2025-06-01 18:18:33 -0500

    Agregando comentarios a las clases

--- logica/clases.py
+++ logica/clases.py
@@ -1,22 +1,29 @@
-class Contacto: 
+''' Se crea la clase Contacto'''
+
+class Contacto:
+
+    '''Creo el constructor con atributos:  nombre , telefono y email.'''
+
     def __init__(self, nombre, telefono, email):
-    
         self.nombre = nombre
         self.telefono = telefono
         self.email = email
-# comienzoc metedo getter y este sirve para leer los valores de un atributo 
-        def get_nombre(self):
+
+    '''Se crea el  método Getter: Este sirve para acceder a los valores de los atributos.'''
+
+    def get_nombre(self):
             return self.nombre
-        def get_telefono (self):
+    def get_telefono (self):
             return self.telefono
-        def get_email (self):
+    def get_email (self):
             return self.email
-# aqui utilizo el metodo setter: este modifica los valores de un atributo
-#utlizo el "nuevo_nombre" como para cuando se quiere colocar un nuevo valor o mejor dicho asignarle otro valor 
-#un ejemplo seria que me van a dar un nuevo valor para remplazar el que ya estabaa y esto tambien sucede con "nuevo_telefono" y "nuevo_email"
-        def set_nombre(self, nuevo_nombre):  
-            self_nombre = nuevo_nombre    
-        def set_telefono (self, nuevo_telefono):
-            self_telefono = nuevo_telefono   
-        def set_email (self, nuevo_email):
-            self_email = nuevo_email
+    
+    '''Se crea el método Setter : Este modifica los valores de los atributos, utlizando  parametros como "nuevo_nombre", "nuevo_telefono" y "nuevo_email" permitiendo
+    que pueda reemplazar el contenido original por estos nuevos valores que se pasaran como argumentos. '''
+
+    def set_nombre(self, nuevo_nombre):  
+            self.nombre = nuevo_nombre    
+    def set_telefono (self, nuevo_telefono):
+            self.telefono = nuevo_telefono   
+    def set_email (self, nuevo_email):
+            self.email = nuevo_email
